44bf50ef1f78fae77c314cd1cef5110473e8fe7a
Giorgi Tskhvedadze <giorgicxvedadze4@gmail.com>
2022-11-09 01:30:45 +0400

    added logics to the again btn

--- script.js
+++ script.js
@@ -11,10 +11,8 @@ console.log(document.querySelector('.guess').value)
 
 */
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-
-document.querySelector('.number').textContent = secretNumber
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -26,9 +24,8 @@ document.querySelector('.check').addEventListener('click', function () {
     // When player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
-
+    document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
-
     document.querySelector('.number').style.width = '30rem';
 
     // When gues is too high
@@ -53,4 +50,24 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   }
-}) 
+})
+
+// Reset the game
+document.querySelector('.again').addEventListener('click', function () {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+
+  // Reset message text and input field
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.guess').value = null;
+
+  // Reset score and secret number
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = secretNumber
+
+  // Reset styles 
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+
+  console.log('again');
+})
